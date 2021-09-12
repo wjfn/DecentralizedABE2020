c005b967e94c2cf0ce3daf976c3d91e29e5c47f3
wjfn <562534074@qq.com>
2021-09-12 10:19:30 +0800

    Update AES.go

--- model/AES/AES.go
+++ model/AES/AES.go
@@ -35,21 +35,26 @@ func AesDecrypt(crypted, key []byte) ([]byte, error) {
 	origData := crypted
 	blockMode.CryptBlocks(origData, crypted)
 	//origData = PKCS5UnPadding(origData)
-	//origData = ZeroUnPadding(origData)
+	origData = ZeroUnPadding(origData)
 	return origData, nil
 }
 
 
 func ZeroPadding(ciphertext []byte, blockSize int) []byte {
 	padding := blockSize - len(ciphertext)%blockSize
-	padtext := bytes.Repeat([]byte{0}, padding)
+	//padtext := bytes.Repeat([]byte{0}, padding)
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
 	return append(ciphertext, padtext...)
 }
 
 func ZeroUnPadding(origData []byte) []byte {
 	length := len(origData)
 	unpadding := int(origData[length-1])
-	return origData[:(length - unpadding)]
+	if(unpadding>=length){
+		return origData
+	}else{
+		return origData[:(length - unpadding)]
+	}
 }
 
 func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
